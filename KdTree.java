6f9ceda9794f61100a11ab54229011853d8c24ac
catbeans <workemailzoop@gmail.com>
2020-09-21 16:04:55 -0400

    draft of PointSET complete

--- KdTree.java
+++ KdTree.java
@@ -1,42 +1,44 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
-
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
-public class KdTree { // set of points in unit square, implemented using 2d-tree
-    public KdTree(){ // construct an empty set of points
+public class KdTree { // set of points in unit square, implemented using 2d-tree; a generalization of a BST to two-dimensional keys
+    public KdTree() { // construct an empty set of points
 
     }
-    public boolean isEmpty(){ // is the set empty?
+
+    public boolean isEmpty() { // is the set empty?
 
     }
-    public int size(){ // number of points in the set
+
+    public int size() { // number of points in the set
 
     }
-    public void insert(Point2D p){ // add the point to the set (if it is not already in the set)
+
+    public void insert(Point2D p) { // add the point to the set (if it is not already in the set)
 
     }
-    public boolean contains(Point2D p){ // does the set contain point p?
+
+    public boolean contains(Point2D p) { // does the set contain point p?
 
     }
-    public void draw(){ // draw all points to standard draw
+
+    public void draw() { // draw all points to standard draw
 
     }
-    public Iterable<Point2D> range(RectHV rect){ // all points that are inside the rectangle (or on the boundary)
+
+    public Iterable<Point2D> range(RectHV rect) { // all points that are inside the rectangle (or on the boundary)
 
     }
-    public Point2D nearest(Point2D p){ // a nearest neighbor in the set to point p; null if the set is empty
+
+    public Point2D nearest(Point2D p) { // a nearest neighbor in the set to point p; null if the set is empty
 
     }
-    public static void main(String[] args){ // unit testing of the methods (optional)
-    /*
-     * KdTreeGenerator
-     * KdTreeVisualizer
-     * NearestNeighborVisualizer
-     */
+
+    public static void main(String[] args) { // unit testing of the methods (optional)
+        /*
+         * KdTreeGenerator
+         * KdTreeVisualizer
+         * NearestNeighborVisualizer
+         */
     }
 }
--- PointSET.java
+++ PointSET.java
@@ -1,9 +1,3 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
-
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
@@ -14,48 +8,72 @@ public class PointSET { // set of points in unit square, implemented using red-b
     public PointSET() { // construct an empty set of points
         points = new SET<Point2D>();
     }
+
     public boolean isEmpty() { // is the set empty?
         return points.isEmpty();
     }
+
     public int size() { // number of points in the set
         return points.size();
     }
-    public void insert(Point2D p) { // add the point to the set (if it is not already in the set)
+
+    public void insert(Point2D p) { // add the point to the set
+        if (p == null) {
+            throw new IllegalArgumentException("Argument can not be null");
+        }
         if (!this.contains(p)) {
             points.add(p);
         }
     }
+
     public boolean contains(Point2D p) { // does the set contain point p?
+        if (p == null) {
+            throw new IllegalArgumentException("Argument can not be null");
+        }
         return points.contains(p);
     }
+
     public void draw() { // draw all points to standard draw
         while (points.iterator().hasNext()) {
             Point2D currPoint = points.iterator().next();
             currPoint.draw();
         }
     }
+
     public Iterable<Point2D> range(RectHV rect) { // all points that are inside the rectangle (or on the boundary)
+        if (rect == null) {
+            throw new IllegalArgumentException("Argument can not be null");
+        }
         SET<Point2D> inRectangle = new SET<Point2D>();
-        inRectangle.intersects(points);
+        for (Point2D point : points) {
+            if (rect.contains(point)) {
+                inRectangle.add(point);
+            }
+        }
         return inRectangle;
     }
-    public Point2D nearest(Point2D p) { // a nearest neighbor in the set to point p; null if the set is empty
-        if (points.size() == 0) {
+
+    public Point2D nearest(Point2D p) { // nearest neighbor in the set to point p
+        if (p == null) {
+            throw new IllegalArgumentException("Argument can not be null");
+        }
+        if (points.size() == 0) { // null if the set is empty
             return null;
         }
-        return p;
+        double minDistance = 1.0;
+        double distanceBetween;
+        Point2D nearestPoint = null;
+        for (Point2D point : points) {
+            distanceBetween = point.distanceTo(p);
+            if (distanceBetween < minDistance) {
+                nearestPoint = point;
+                minDistance = distanceBetween;
+            }
+        }
+        return nearestPoint;
     }
-    public static void main(String[] args) { // unit testing of the methods (optional)
-    /*
-    Corner cases
-        Throw an IllegalArgumentException if any argument is null
-    Performance requirements
-        Support insert() and contains() in time proportional to the logarithm of the number of points in the set in the worst case
-        Support nearest() and range() in time proportional to the number of points in the set
 
-    * KdTreeGenerator
-    * KdTreeVisualizer
-    * NearestNeighborVisualizer
-    */
+    public static void main(String[] args) { // unit testing of the methods (optional)
+
     }
 }
